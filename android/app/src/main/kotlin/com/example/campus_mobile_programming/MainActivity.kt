0db77f038cfd657ddcc0107d57453b8541cd4c95
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-10-21 11:17:53 +0700

    init flutter project

---
+++ android/app/src/main/kotlin/com/example/campus_mobile_programming/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.campus_mobile_programming
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int nilai = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Counter App')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("$nilai", style: const TextStyle(fontSize: 48)),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        nilai--;
+                      });
+                    },
+                    child: const Text('-'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        nilai += 1;
+                      });
+                    },
+                    child: const Text('+'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
